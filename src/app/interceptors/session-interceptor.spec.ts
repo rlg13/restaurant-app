8d8b7ed173f57a84a4e3beb81fc4ebc66731f5c2
Rafael Lopez <rafaellopez83@gmail.com>
2019-07-18 20:43:39 +0200

    Fix session interceptor test

--- src/app/interceptors/session-interceptor.spec.ts
+++ src/app/interceptors/session-interceptor.spec.ts
@@ -1,7 +1,62 @@
-import { SessionInterceptor } from './session-interceptor';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { Dish } from 'src/app/model/dish';
 
-describe('SessionInterceptor', () => {
-  it('should create an instance', () => {
-    expect(new SessionInterceptor()).toBeTruthy();
+import { DishType } from './../model/dish-type.enum';
+import { DishService } from './../services/dish.service';
+import { SessionInterceptor } from './session-interceptor';
+import { Router } from '@angular/router';
+
+class RouterStub {
+  public navigate(routes: string[]): void {}
+}
+
+describe('SessionInterceptor tests', () => {
+  let service: DishService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        DishService,
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: SessionInterceptor,
+          multi: true
+        },
+        {
+          provide: Router,
+          useClass: RouterStub,
+          deps: []
+        }
+      ]
+    });
+
+    service = TestBed.get(DishService);
+    httpMock = TestBed.get(HttpTestingController);
+  });
+
+
+  it('should add header Authorization', () => {
+    localStorage.setItem('Authorization','some value');
+
+    const DISH_TYPE = DishType.FIRST;
+
+    const dummyDish = [
+      new Dish({ id: 1, type: DISH_TYPE, name: 'Salad' })
+    ];
+
+    const mockResponse = { status: 200, statusText: 'Ok' };
+
+    service.findByType(DISH_TYPE).subscribe(response => {
+      expect(response).toBeTruthy();
+    });
+
+    const httpRequest = httpMock.expectOne(`${DishService.DISH_TYPE_ENDPOINT}${DISH_TYPE}`);
+    expect(httpRequest.request.headers.has('Authorization')).toEqual(true);
+    expect(httpRequest.request.method).toEqual('GET');
+    httpRequest.flush(dummyDish, mockResponse);
   });
 });
